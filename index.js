fa5a6da47c6e6ba6a49a4f00e25b753d14d0499f
xiangshouding <fansekey@gmail.com>
2015-05-26 19:00:32 +0800

    support less 2.x

--- index.js
+++ index.js
@@ -5,26 +5,25 @@
 
 'use strict';
 
-
 var less = require('less');
 var root = fis.project.getProjectPath();
 
 module.exports = function(content, file, conf){
+
     conf.paths = [ file.dirname, root ];
-    if (conf.syncImport === undefined) conf.syncImport = true;
-    if (conf.relativeUrls === undefined) conf.relativeUrls = true;
-    var parser = new(less.Parser)(conf);
-    parser.parse(content, function (err, tree) {
-        if(err){
+    conf.syncImport = true;
+    conf.relativeUrls = true;
+
+    less.render(content, conf, function (err, result) {
+        if (err) {
             throw err;
-        } else {
-            if(parser.imports){
-                fis.util.map(parser.imports.files, function(path){
-                    file.cache.addDeps(path);
-                });
-            }
-            content = tree.toCSS(conf);
+            return;
         }
+        content = result.css;
+        result.imports.forEach(function (path) {
+            file.cache.addDeps(path);
+        });
     });
-	return content;
+
+  return content;
 };
